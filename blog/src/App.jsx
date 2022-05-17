f1b751411ed9ca4f715fb2378f1f5ebbce9977d7
Toan Tran <toantdfu@gmail.com>
2022-05-17 10:20:06 +0900

    useQuery, pagination and prefetching data with react-query

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -1,13 +1,22 @@
-import { Posts } from "./Posts";
-import "./App.css";
+import { Posts } from './Posts';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import './App.css';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     // provide React Query client to App
-    <div className="App">
-      <h1>Blog Posts</h1>
-      <Posts />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+
+        <Posts />
+      </div>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- blog/src/Posts.jsx
+++ blog/src/Posts.jsx
@@ -1,21 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 
-import { PostDetail } from "./PostDetail";
+import { PostDetail } from './PostDetail';
 const maxPostPage = 10;
 
-async function fetchPosts() {
+async function fetchPosts(page) {
   const response = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=10&_page=0"
+    `https://jsonplaceholder.typicode.com/posts?_limit=10&_page=${page}`
   );
   return response.json();
 }
 
 export function Posts() {
-  const [currentPage, setCurrentPage] = useState(0);
+  const queryClient = useQueryClient();
+
+  const [currentPage, setCurrentPage] = useState(1);
   const [selectedPost, setSelectedPost] = useState(null);
 
-  // replace with useQuery
-  const data = [];
+  useEffect(() => {
+    if (currentPage < maxPostPage) {
+      const nextPage = currentPage + 1;
+      queryClient.prefetchQuery(['posts', nextPage], () =>
+        fetchPosts(nextPage)
+      );
+    }
+  }, [currentPage, queryClient]);
+
+  let { data, error, isError, isLoading, isPreviousData } = useQuery(
+    ['posts', currentPage],
+    () => fetchPosts(currentPage),
+    {
+      keepPreviousData: true,
+      staleTime: 2000,
+    }
+  );
+
+  if (isLoading) {
+    return <span>Loading ...</span>;
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
   return (
     <>
@@ -24,18 +50,25 @@ export function Posts() {
           <li
             key={post.id}
             className="post-title"
-            onClick={() => setSelectedPost(post)}
-          >
+            onClick={() => setSelectedPost(post)}>
             {post.title}
           </li>
         ))}
       </ul>
       <div className="pages">
-        <button disabled onClick={() => {}}>
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}>
           Previous page
         </button>
-        <span>Page {currentPage + 1}</span>
-        <button disabled onClick={() => {}}>
+        <span>Page {currentPage}</span>
+        <button
+          disabled={isPreviousData || currentPage === maxPostPage}
+          onClick={() => {
+            if (!isPreviousData) {
+              setCurrentPage(currentPage + 1);
+            }
+          }}>
           Next page
         </button>
       </div>
