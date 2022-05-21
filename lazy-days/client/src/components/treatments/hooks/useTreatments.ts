372d9312d31f3766a4b656444d442e025969c0ac
Toan Tran <toantdfu@gmail.com>
2022-05-22 06:59:56 +0900

    Centralize onError handling on queryClient with defaultOptions

--- lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 async function getTreatments(): Promise<Treatment[]> {
   const { data } = await axiosInstance.get('/treatments');
@@ -11,13 +10,6 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  const toast = useCustomToast();
-  const { data = [] } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title = error instanceof Error ? error.message : 'Error';
-
-      toast({ title, status: 'error' });
-    },
-  });
+  const { data = [] } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
