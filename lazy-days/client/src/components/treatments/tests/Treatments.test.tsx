a02027879416cbedea0cfe778d6994071bb03528
Toan Tran <toantdfu@gmail.com>
2022-06-27 06:23:14 +0900

    Setup test with react-testing-library

--- lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
+++ lazy-days/client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,9 @@
 import { screen } from '@testing-library/react';
+import { renderWithQueryClient } from 'test-utils';
 
 import { Treatments } from '../Treatments';
 
 test('renders response from query', () => {
   // write test here
+  renderWithQueryClient(<Treatments />);
 });
--- lazy-days/client/src/test-utils/index.tsx
+++ lazy-days/client/src/test-utils/index.tsx
@@ -1,6 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
+import { ReactElement } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 // import { defaultQueryClientOptions } from '../react-query/queryClient';
+
+export const renderWithQueryClient = (
+  children: ReactElement,
+  client?: QueryClient,
+): RenderResult => {
+  const queryClient = client ?? new QueryClient();
+  return render(
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>,
+  );
+};
 
 // from https://tkdodo.eu/blog/testing-react-query#for-custom-hooks
 // export const createQueryClientWrapper = (): React.FC => {
